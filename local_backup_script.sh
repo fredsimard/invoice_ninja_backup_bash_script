24beff489cc7daac2803d597bde0c2f286257d7a
fredsimard <frederic.simard@me.com>
2020-05-19 18:12:18 -0400

    Update local_backup_script.sh

--- local_backup_script.sh
+++ local_backup_script.sh
@@ -1,5 +1,62 @@
 #!/bin/bash
 
-rsync -avz -e "ssh -p ___REMOTE_HOST_PORT_NUMBER__ -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress __FULL_PATH_TO_LOCAL_BACKUPS_FOLDER__ __USERNAME__@__IPADDRESS__:__FULL_PATH_TO_REMOTE_BACKUPS_FOLDER__
+################################################################
+
+export PATH=/bin:/usr/bin:/usr/local/bin
+TODAY=$(date +%Y-%m-%d)  ## Current date formatted as : 2020-12-01 — If you change the format, also change it below for the remove older backups part of the script.
+HOSTNAME=$(hostname -s)  ## Or put whatever name you want in quotes instead of $(...)
+
+################################################################
+################## Update below values  ########################
+
+DB_BACKUP_PATH='/root/Backups'  ## This is the path where the local backups will be stored
+WEBSITE_BACKUP_PATH='/var/www/html/invoiceninja/'  ## The path to the web app
+WEBSITE_ARCHIVE_FILE="invoice-ninja-website-$TODAY.tar.gz"  ## The name of the archive for the web app
+MYSQL_HOST='localhost'
+MYSQL_PORT='3306'
+MYSQL_USER='root' ## or whatever user that has the sufficient privileges
+MYSQL_PASSWORD='your_user_password'
+DATABASE_NAME='invoiceninja' ## or whatever name you gave to the DB
+BACKUP_RETAIN_DAYS=30   ## Number of days to keep local backup copy
+
+#################################################################
+
+mkdir -p ${DB_BACKUP_PATH}/${TODAY}
+echo "Backup started for SQL database \'${DATABASE_NAME}\' to ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}-${TODAY}.sql.gz"
+
+mysqldump -h ${MYSQL_HOST} \
+   -P ${MYSQL_PORT} \
+   -u ${MYSQL_USER} \
+   -p${MYSQL_PASSWORD} \
+   ${DATABASE_NAME} | gzip > ${DB_BACKUP_PATH}/${TODAY}/${DATABASE_NAME}-${TODAY}.sql.gz
+if [ $? -eq 0 ]; then
+  echo "Database backup successfully completed"
+else
+  echo "Error found during backup"
+  exit 1
+fi
+
+##### Backup invoice ninja folder  #####
+
+#print start status message
+echo "Backing up $WEBSITE_BACKUP_PATH to $DB_BACKUP_PATH/$WEBSITE_ARCHIVE_FILE"
+
+# Backup the files using tar
+tar czf ${DB_BACKUP_PATH}/${TODAY}/${WEBSITE_ARCHIVE_FILE} ${WEBSITE_BACKUP_PATH}
+
+# print end message
+echo
+echo "Backup finished"
+
+##### Remove backups older than {BACKUP_RETAIN_DAYS} days  #####
+
+DBDELDATE=$(date -d "${BACKUP_RETAIN_DAYS} days ago" +%Y-%m-%d) ## if you changed the format for the TODAY variable, also change it here.
+
+if [ ! -z ${DB_BACKUP_PATH} ]; then
+      cd ${DB_BACKUP_PATH}
+      if [ ! -z ${DBDELDATE} ] && [ -d ${DBDELDATE} ]; then
+            rm -rf ${DBDELDATE}
+      fi
+fi
 
 ### End of script ####
