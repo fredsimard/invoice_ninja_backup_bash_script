dac58dee7494b307360ecd69ee8c084badb5da99
fredsimard <frederic.simard@me.com>
2020-05-23 14:02:25 -0400

    Update remote_backup_script.sh

--- remote_backup_script.sh
+++ remote_backup_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -avz -e "ssh -p ___REMOTE_HOST_PORT_NUMBER__ -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress __FULL_PATH_TO_LOCAL_BACKUPS_FOLDER__ __USERNAME__@__IPADDRESS__:__FULL_PATH_TO_REMOTE_BACKUPS_FOLDER__
+rsync -avz -e "ssh -p __REMOTE_HOST_PORT_NUMBER__ -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress __FULL_PATH_TO_LOCAL_BACKUPS_FOLDER__ __USERNAME__@__IPADDRESS__:__FULL_PATH_TO_REMOTE_BACKUPS_FOLDER__
 
 ### End of script ####
